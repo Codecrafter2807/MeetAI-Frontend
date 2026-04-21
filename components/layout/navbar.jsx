ed226e4ac42dc9ec49b4fbc928879722e037e178
Codecrafter2807 <codecrafter.2807@gmail.com>
2026-04-22 00:54:41 +0530

    feat: implement landing page with feature showcase and responsive navigation bar

--- components/layout/navbar.jsx
+++ components/layout/navbar.jsx
@@ -72,9 +72,9 @@ export function Navbar() {
 
         {/* Mobile Logo */}
         <Link href="/" className="flex items-center lg:hidden">
-          <div className="flex h-12 w-44 items-center justify-start overflow-hidden">
-            <img src="/logo.png" alt="MeetAI" className="h-full w-full object-contain scale-110 transition-transform duration-300 dark:hidden" />
-            <img src="/logo_d.png" alt="MeetAI" className="hidden h-full w-full object-contain scale-110 transition-transform duration-300 dark:block" />
+          <div className="flex h-10 w-28 items-center justify-start overflow-hidden">
+            <img src="/logo.png" alt="MeetAI" className="h-full w-full object-contain dark:hidden" />
+            <img src="/logo_d.png" alt="MeetAI" className="hidden h-full w-full object-contain dark:block" />
           </div>
         </Link>
 
